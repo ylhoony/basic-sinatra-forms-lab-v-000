9580917154e72bdc05b55eadb4f3248f79ed9953
Hoon Lee <yl.hoony@gmail.com>
2018-01-05 07:51:04 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -9,6 +9,13 @@ class App < Sinatra::Base
   post '/team' do
     # puts params
     # @info
+    @name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @pf = params[:pf]
+    @sf = params[:sf]
+    @c = params[:c]
 
     erb :team
   end
